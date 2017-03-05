b21c28b6101ae8e06a8669c659420939baa7fb08
Khoa Le <kdl2483@vh222004.truman.edu>
2017-03-04 20:40:07 -0600

    Finished the project | TODO spend something go over the requirement list and refactor code

--- Shared_Memory/chat0.c
+++ Shared_Memory/chat0.c
@@ -4,54 +4,63 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
+
+/* To get strnlen(), memcpy(), etc. */
 #include <string.h> 
-#include <semaphore.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /* To get perror() */
 #include <errno.h>
-sem_t * sem_id;
 
 /* Shared definitions */
 #include "shm.h"
 
+/* semaphore library */
+#include <semaphore.h>
+
 int main (int argc, char **argv)
 {
-  
-  int memory_handle = shm_open (segment_name, O_RDWR, 0);
-  if (memory_handle == -1) {
-    perror ("shm_open");
-    exit (-1);
-  }
+ int memory_handle = shm_open (segment_name, 
+				O_RDWR | O_CREAT | O_TRUNC, 
+				S_IRWXU);
+ /* Check for errors */
+ if (memory_handle == -1) {
+   perror("shm_open");
+   exit (-1);
+ }
 
- /* Map shared memory segment into this process's address space. */
-  void *segment_addr = mmap (NULL, 
-           sizeof (SHARED_MEM),
-           PROT_READ | PROT_WRITE,
-           MAP_SHARED, 
-           memory_handle,
-           0);
-  if (segment_addr == (void *) -1) {
+ if (ftruncate (memory_handle, sizeof(SHARED_MEM)) == -1) {
+   perror ("ftruncate");
+   exit (-1);
+ }
+
+ void *segment_addr = mmap (NULL,sizeof(SHARED_MEM), PROT_WRITE | PROT_READ, MAP_SHARED, memory_handle,0);
+
+ if (segment_addr == (void *) -1) {
     perror ("mmap");
     exit (-1);
+ }
+
+ SHARED_MEM *chunk = (SHARED_MEM *) segment_addr;
+ sem_init(&chunk->sema, 1, 1);
+ sem_init(&chunk->sema1, 1, 0);
+ 
+ while (1) {
+  sem_wait(&chunk->sema);
+  if((chunk->mesg_size) != 0) {
+   printf("Chat1 says: %s\n", chunk->mesg);
   }
-
-  SHARED_MEM *chunk = (SHARED_MEM *) segment_addr;
-
-  printf ("Shared memory contained a string of %d characters:\n",
-    chunk->mesg_size);
-  printf ("\"%s\"\n", chunk->mesg);
-
-  if (munmap (segment_addr, sizeof (SHARED_MEM)) == -1) {
-    perror ("munmap");
-    exit (-1);
+  printf ("Please enter some text to send to 1 (max %d chars): ", MAX_MESG_SIZE);
+  fgets (chunk->mesg, 1024, stdin);
+  chunk->mesg_size = strlen (chunk->mesg);
+  if (strcmp("END\n", chunk->mesg) == 0) {
+   exit(0);
   }
+  sem_post(&chunk->sema1);
+    
+ }
 
-  if (shm_unlink (segment_name) == -1) {
-    perror ("shm_unlink");
-    exit (-1);
-  }
-
-  exit(0);
+  exit (0);
 }
--- Shared_Memory/chat1.c
+++ Shared_Memory/chat1.c
@@ -1,59 +1,59 @@
-/* For shm* functions */
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <string.h> 
-#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-/* To get perror() */
 #include <errno.h>
+#include <semaphore.h>
 
-/* Shared definitions */
 #include "shm.h"
-//sem_t * sem_id;
-
-/* POSIX is part of the linux "real-time" library, so you need to
-   compile with -lrt. */
 int main (int argc, char **argv)
 {
-  
-  int memory_handle = shm_open (segment_name, 
-				O_RDWR | O_CREAT | O_TRUNC, 
-				S_IRWXU);
-  /* Check for errors */
+  int memory_handle = shm_open (segment_name, O_RDWR, 0);
   if (memory_handle == -1) {
-    perror("shm_open");
+    perror ("shm_open");
     exit (-1);
   }
 
-  if (ftruncate (memory_handle, sizeof(SHARED_MEM)) == -1) {
-    perror ("ftruncate");
-    exit (-1);
-  }
-
-  void *segment_addr = mmap (NULL,
-			     sizeof (SHARED_MEM), 
-			     PROT_WRITE | PROT_READ, 
-			     MAP_SHARED,
+  void *segment_addr = mmap (NULL, 
+			     sizeof (SHARED_MEM),
+			     PROT_READ | PROT_WRITE,
+			     MAP_SHARED, 
 			     memory_handle,
 			     0);
-
   if (segment_addr == (void *) -1) {
     perror ("mmap");
     exit (-1);
   }
-
-  /* At this point, we have a chunk of shared memory.  To access it,
-     we have to superimpose some structure on it. */
+ 
   SHARED_MEM *chunk = (SHARED_MEM *) segment_addr;
+  
+ while(1) {
+    sem_wait(&chunk->sema1);
+    printf ("Chat0 says: %s\n", chunk->mesg);
+    printf ("Please enter some text to send to 0 (max %d chars): ", MAX_MESG_SIZE);
+    fgets(chunk->mesg, 1024, stdin);
+    chunk->mesg_size = strlen (chunk->mesg);
+    if (strcmp(chunk->mesg, "END\n") == 0) {
+     exit(0);
+    }
+    sem_post(&chunk->sema); 
+ }
 
-  printf ("Please enter some text (max %d chars): ", MAX_MESG_SIZE);
-  gets (chunk->mesg);
-  chunk->mesg_size = strlen (chunk->mesg);
+  if (munmap (segment_addr, sizeof (SHARED_MEM)) == -1) {
+    perror ("munmap");
+    exit (-1);
+  }
 
-  exit (0);
+  /* Free the shared memory segment. Shm_unlink() just schedules the
+     deletion of the segment. It will actually be deleted only after
+     the last process that has mmap()'d to it munmap()'s it. */
+  if (shm_unlink (segment_name) == -1) {
+    perror ("shm_unlink");
+    exit (-1);
+  }
+
+  exit(0);
 }
+  
