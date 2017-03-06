2af5d97d00ca3df28d6cdf3dd14b904517da49c5
Khoa Le <khoadang.le@outlook.com>
2017-03-06 09:57:03 -0600

    code refactoring

--- Shared_Memory/chatProgram/chat0.c
+++ Shared_Memory/chatProgram/chat0.c
@@ -28,61 +28,61 @@
 
 int main (int argc, char **argv)
 {
- int memory_handle = shm_open (segment_name, 
-				O_RDWR | O_CREAT | O_TRUNC, 
-				S_IRWXU);
- /* Check for errors */
- if (memory_handle == -1) {
-   perror("shm_open");
-   exit (-1);
- }
+   int memory_handle = shm_open (segment_name, 
+          O_RDWR | O_CREAT | O_TRUNC, 
+          S_IRWXU);
+   /* Check for errors */
+   if (memory_handle == -1) {
+     perror("shm_open");
+     exit (-1);
+   }
 
- if (ftruncate (memory_handle, sizeof(SHARED_MEM)) == -1) {
-   perror ("ftruncate");
-   exit (-1);
- }
+   if (ftruncate (memory_handle, sizeof(SHARED_MEM)) == -1) {
+     perror ("ftruncate");
+     exit (-1);
+   }
 
- void *segment_addr = mmap (NULL,
-			    sizeof(SHARED_MEM), 
-			    PROT_WRITE | PROT_READ, 
-                            MAP_SHARED, 
-                            memory_handle, 
-                            0);
+   void *segment_addr = mmap (NULL,
+                              sizeof(SHARED_MEM), 
+                              PROT_WRITE | PROT_READ, 
+                              MAP_SHARED, 
+                              memory_handle, 
+                              0);
 
- if (segment_addr == (void *) -1) {
-    perror ("mmap");
-    exit (-1);
- }
+   if (segment_addr == (void *) -1) {
+      perror ("mmap");
+      exit (-1);
+   }
 
- SHARED_MEM *chunk = (SHARED_MEM *) segment_addr;
- sem_init(&chunk->sema, 1, 1);
- sem_init(&chunk->sema1, 1, 0);
- 
- while (1) {
-  sem_wait(&chunk->sema);
-  
-  if((chunk->mesg_size) != 0) {
-   printf("Chat1 says: %s\n", chunk->mesg);
-  }
-  if (strcmp("END\n", chunk->mesg) == 0) {
-   break;
-  }
-  printf ("Please enter some text to send to 1 (max %d chars): ", MAX_MESG_SIZE);
-  fgets (chunk->mesg, 1024, stdin);
-  
-  chunk->mesg_size = strlen (chunk->mesg);
-  if (strcmp("END\n", chunk->mesg) == 0) {
-   sem_post(&chunk->sema1);
-   break;
-  }
-  
-  sem_post(&chunk->sema1);  
- }
- 
- if (munmap (segment_addr, sizeof (SHARED_MEM)) == -1) {
-    perror ("munmap");
-    exit (-1);
-  }
+   SHARED_MEM *chunk = (SHARED_MEM *) segment_addr;
+   sem_init(&chunk->sema, 1, 1);
+   sem_init(&chunk->sema1, 1, 0);
+   
+   while (1) {
+    sem_wait(&chunk->sema);
+    
+    if((chunk->mesg_size) != 0) {
+     printf("Chat1 says: %s\n", chunk->mesg);
+    }
+    if (strcmp("END\n", chunk->mesg) == 0) {
+     break;
+    }
+    printf ("Please enter some text to send to 1 (max %d chars): ", MAX_MESG_SIZE);
+    fgets (chunk->mesg, 1024, stdin);
+    
+    chunk->mesg_size = strlen (chunk->mesg);
+    if (strcmp("END\n", chunk->mesg) == 0) {
+     sem_post(&chunk->sema1);
+     break;
+    }
+    
+    sem_post(&chunk->sema1);  
+   }
+   
+   if (munmap (segment_addr, sizeof (SHARED_MEM)) == -1) {
+      perror ("munmap");
+      exit (-1);
+    }
 
- exit (0);
+   exit (0);
 }
